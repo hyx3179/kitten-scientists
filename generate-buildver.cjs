3c40b7eed3c195dacbf3a9692b38017c0e314f3e
hyx3179 <hyx3179@gmail.com>
2025-03-16 23:13:42 +0800

    更新部署工作流

--- generate-buildver.cjs
+++ generate-buildver.cjs
@@ -1,12 +1,12 @@
-var fs = require("fs");
-var FILE_NAME = "./public/ks.version.json";
+const fs = require("node:fs");
+const FILE_NAME = "./public/ks.version.json";
 
 console.log("Incrementing build number...");
-fs.readFile(FILE_NAME, function (err, content) {
+fs.readFile(FILE_NAME, (err, content) => {
   if (err) throw err;
-  var metadata = JSON.parse(content);
+  const metadata = JSON.parse(content);
   metadata.KSbuildRevision = metadata.KSbuildRevision + 1;
-  fs.writeFile(FILE_NAME, JSON.stringify(metadata), function (err) {
+  fs.writeFile(FILE_NAME, JSON.stringify(metadata), err => {
     if (err) throw err;
     console.log(`Current build number: ${metadata.KSbuildRevision}`);
   });
